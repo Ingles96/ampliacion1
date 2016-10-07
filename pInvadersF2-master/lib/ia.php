d3499cf83d491ab19e0bb3583b5aa43e53699b8d
Josep Ingles Arenes <joinar@floridauniversitaria.es>
2016-10-07 08:32:00 +0200

    Proyecto Nave2

---
+++ pInvadersF2-master/lib/ia.php
@@ -0,0 +1,26 @@
+<?php
+
+class ia
+{
+public $columna = '0';
+public $fila = '3';
+
+public function setColumna($col) {
+$this->columna = $col;
+}
+public function getcolumna() {
+
+return $this->columna;
+}
+public function setFila($fil) {
+$this->fila = $fil;
+}
+public function getfila() {
+
+  return $this->fila;
+}
+  public function RandomPos() {
+}
+
+}
+ ?>
